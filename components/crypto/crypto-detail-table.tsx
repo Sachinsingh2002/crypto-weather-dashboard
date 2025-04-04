3fa3fe9a0005c13da2120136f1a4c884d6042c50
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2025-04-04 16:47:52 +0530

    detail table*

--- components/crypto/crypto-detail-table.tsx
+++ components/crypto/crypto-detail-table.tsx
@@ -5,6 +5,7 @@ interface CryptoDetailTableProps {
   data: CryptoDetail
 }
 
+
 export default function CryptoDetailTable({ data }: CryptoDetailTableProps) {
   const formatNumber = (value: number, options: Intl.NumberFormatOptions = {}) => {
     return new Intl.NumberFormat("en-US", options).format(value)
