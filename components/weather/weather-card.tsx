447e68e59d45826715f782c5e9742e4ffcc9ad6d
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2025-04-04 16:48:46 +0530

    weather card..

--- components/weather/weather-card.tsx
+++ components/weather/weather-card.tsx
@@ -2,6 +2,7 @@
 
 import type React from "react"
 
+
 import Link from "next/link"
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
